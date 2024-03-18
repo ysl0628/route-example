61bd9689a5a6531830705195e44c115746d959ea
ysl0628 <yihsinlan@gmail.com>
2024-03-18 15:41:24 +0800

    feat: update LinkWithIcon to pass additional props

--- components/LinkWIthIcon/page.tsx
+++ components/LinkWIthIcon/page.tsx
@@ -5,9 +5,10 @@ type LinkWithIconProps = {
   path: string;
 };
 
-const LinkWithIcon = ({ title, path }: LinkWithIconProps) => {
+const LinkWithIcon = ({ title, path, ...props }: LinkWithIconProps) => {
   return (
     <Link
+      {...props}
       className="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
       href={path}
     >
