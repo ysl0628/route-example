f230f3b32560c01a0e58dd6bdecd5ba123e06be2
ysl0628 <yihsinlan@gmail.com>
2024-03-20 10:27:57 +0800

    style: modify LinkWithIcon layout

---
+++ components/LinkWIthIcon/index.tsx
@@ -0,0 +1,38 @@
+import Link, { LinkProps } from "next/link";
+
+type LinkWithIconProps = {
+  title: string;
+  path: string;
+} & React.AnchorHTMLAttributes<HTMLAnchorElement> &
+  Partial<LinkProps>;
+
+const LinkWithIcon = ({ title, path, ...props }: LinkWithIconProps) => {
+  return (
+    <div>
+      <Link
+        {...props}
+        className="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        href={path}
+      >
+        {title}
+        <svg
+          className="w-4 h-4 ms-2 rtl:rotate-180"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 10"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M1 5h12m0 0L9 1m4 4L9 9"
+          />
+        </svg>
+      </Link>
+    </div>
+  );
+};
+
+export default LinkWithIcon;
