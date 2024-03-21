3ed8afc191bbba1f6628f32a1b01a0c3a79d4338
ysl0628 <yihsinlan@gmail.com>
2024-03-21 22:11:56 +0800

    chore: fix formatting and quotes in route.ts

--- app/(navigation)/redirect/trigger/route.ts
+++ app/(navigation)/redirect/trigger/route.ts
@@ -1,13 +1,13 @@
-import { redirect } from "next/navigation";
-import { NextResponse, NextRequest } from "next/server";
+import { redirect } from 'next/navigation'
+import { NextResponse, NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
-  const query = searchParams.get("q");
-  
-  if (query === "helloworld") {
-   redirect("/redirect/redirected");
+  const searchParams = request.nextUrl.searchParams
+  const query = searchParams.get('q')
+
+  if (query === 'helloworld') {
+    redirect('/redirect/redirected')
   }
 
-  return NextResponse.json({ message: "successed" });
+  return NextResponse.json({ message: 'succeed' })
 }
