c7e52afae3bdcd1d3008fd927d07f913ca31d5bb
Renee Lan <yihsinlan@gmail.com>
2024-05-25 17:58:26 +0800

    fix: modify date data

--- app/api/data-fetching/route.ts
+++ app/api/data-fetching/route.ts
@@ -3,8 +3,7 @@ import { NextResponse } from 'next/server'
 export const revalidate = 0
 
 export async function GET() {
-  const date = new Date()
   // 可序列化的日期
-  const serializableDate = JSON.stringify(date.valueOf())
+  const serializableDate = JSON.stringify(Date.now())
   return NextResponse.json({ message: 'succeed', data: serializableDate })
 }
