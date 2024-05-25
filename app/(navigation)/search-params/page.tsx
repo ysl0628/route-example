008c03071ee716efe0d2b633dec200e8d9c17f52
ysl0628 <yihsinlan@gmail.com>
2024-05-25 15:53:28 +0800

    refactor: optimize imports in search-params/page.tsx

--- app/(navigation)/search-params/page.tsx
+++ app/(navigation)/search-params/page.tsx
@@ -1,15 +1,39 @@
-"use client";
+'use client'
 
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams } from 'next/navigation'
 
-import Link from "next/link";
-import PageCard from "@/components/PageCard";
-import LinkWithIcon from "@/components/LinkWIthIcon";
+import Link from 'next/link'
+import PageCard from '@/components/PageCard'
+import LinkWithIcon from '@/components/LinkWIthIcon'
+import { Suspense } from 'react'
+
+const SearchParamsFallback = () => {
+  return <>Loading...</>
+}
+
+const SearchParamsComponent = () => {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+
+  return (
+    <>
+      {searchParams.size === 0 ? (
+        <LinkWithIcon
+          title="點擊取得 searchParams"
+          path="/search-params?q=helloworld"
+        />
+      ) : (
+        <LinkWithIcon title="去除 searchParams" path="/search-params" />
+      )}
+      <div>
+        <div>pathname: {pathname}</div>
+        <div>searchParams: {searchParams.get('q')}</div>
+      </div>
+    </>
+  )
+}
 
 const Page = () => {
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-
   return (
     <PageCard
       title="useSearchParams"
@@ -17,19 +41,9 @@ const Page = () => {
       url="/search-params"
     >
       <div className="flex flex-col gap-2">
-        {searchParams.size === 0 ? (
-          <LinkWithIcon
-            title="點擊取得 searchParams"
-            path="/search-params?q=helloworld"
-          />
-        ) : (
-          <LinkWithIcon title="去除 searchParams" path="/search-params" />
-        )}
-        <div>
-          <div>pathname: {pathname}</div>
-          <div>searchParams: {searchParams.get("q")}</div>
-        </div>
-
+        <Suspense fallback={<SearchParamsFallback />}>
+          <SearchParamsComponent />
+        </Suspense>
         <div className="flex gap-5">
           <Link href="/" className="underline-offset-2 underline">
             回到首頁
@@ -37,7 +51,7 @@ const Page = () => {
         </div>
       </div>
     </PageCard>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
