f3a09526e2e1ca91bb7aab10cdb022e4293a58a4
ysl0628 <yihsinlan@gmail.com>
2024-05-25 23:26:45 +0800

    fix: update data fetching endpoint in config/index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 const API_ENDPOINT = process.env.NEXT_PUBLIC_VERCEL_URL
 const FETCHING_ENDPOINT_URL =
   process.env.NODE_ENV === 'production'
-    ? `https://${process.env.VERCEL_URL}/api/data-fetching`
+    ? `https://ysl0628-app-router-playground.vercel.app/api/data-fetching`
     : 'http://localhost:3000/api/data-fetching'
 
 export { API_ENDPOINT, FETCHING_ENDPOINT_URL }
