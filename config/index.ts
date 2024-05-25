0d4e2eba919c30d8912f74e282b73f9f20c9d8cd
ysl0628 <yihsinlan@gmail.com>
2024-05-25 17:04:13 +0800

    refactor: update FETCHING_ENDPOINT_URL in config/index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,6 @@
 const API_ENDPOINT = process.env.NEXT_PUBLIC_VERCEL_URL
-const FETCHING_ENDPOINT_URL =
-  process.env.NODE_ENV === 'production'
-    ? `https://${API_ENDPOINT}/api/data-fetching`
-    : `${API_ENDPOINT}/api/data-fetching`
+const FETCHING_ENDPOINT_URL = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/data-fetching`
+  : 'http://localhost:3000/api/data-fetching'
 
 export { API_ENDPOINT, FETCHING_ENDPOINT_URL }
